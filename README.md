cf6556060a9cd9f1e0d1f7c8b6fa9c307579767d
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2026-05-04 22:08:24 +0200

    first person view (press 2)

--- README.md
+++ README.md
@@ -1,36 +1,23 @@
 # TrackBuilder
 
-A [libGDX](https://libgdx.com/) project generated with [gdx-liftoff](https://github.com/libgdx/gdx-liftoff).
+Test of constructing a race track model from a spline which is defined by a few control points.
 
-This project was generated with a template including simple application launchers and an `ApplicationAdapter` extension that draws libGDX logo.
+The control points are manually defined in the code.
+It can include height differences by using different Y values for the control points.
+
+Assumes a closed loop, this is easy to adapt.
+With very sharp corners the texture may get distorted. Avoid those.
+The centre line is modeled as a series of white rectangles floating slightly above the road surface to avoid Z-fighting.
+
+A tilt angle for the road can be defined with a separate list of control points with the normal vector.
+
+Press 1 for map view, press 2 for first person view.
+
+
+
 
 ## Platforms
 
 - `core`: Main module with the application logic shared by all platforms.
 - `lwjgl3`: Primary desktop platform using LWJGL3; was called 'desktop' in older docs.
 - `teavm`: Web backend that supports most JVM languages.
-
-## Gradle
-
-This project uses [Gradle](https://gradle.org/) to manage dependencies.
-The Gradle wrapper was included, so you can run Gradle tasks using `gradlew.bat` or `./gradlew` commands.
-Useful Gradle tasks and flags:
-
-- `--continue`: when using this flag, errors will not stop the tasks from running.
-- `--daemon`: thanks to this flag, Gradle daemon will be used to run chosen tasks.
-- `--offline`: when using this flag, cached dependency archives will be used.
-- `--refresh-dependencies`: this flag forces validation of all dependencies. Useful for snapshot versions.
-- `build`: builds sources and archives of every project.
-- `cleanEclipse`: removes Eclipse project data.
-- `cleanIdea`: removes IntelliJ project data.
-- `clean`: removes `build` folders, which store compiled classes and built archives.
-- `eclipse`: generates Eclipse project data.
-- `idea`: generates IntelliJ project data.
-- `lwjgl3:jar`: builds application's runnable jar, which can be found at `lwjgl3/build/libs`.
-- `lwjgl3:run`: starts the application.
-- `teavm:build`: builds the JavaScript application into the build/dist/webapp folder.
-- `teavm:run`: serves the JavaScript application at http://localhost:8080 via a local Jetty server.
-- `test`: runs unit tests (if any).
-
-Note that most tasks that are not specific to a single project can be run with `name:` prefix, where the `name` should be replaced with the ID of a specific project.
-For example, `core:clean` removes `build` folder only from the `core` project.
--- core/src/main/java/com/monstrous/trackbuilder/Main.java
+++ core/src/main/java/com/monstrous/trackbuilder/Main.java
@@ -2,16 +2,16 @@ package com.monstrous.trackbuilder;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.CatmullRomSpline;
@@ -24,15 +24,21 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
-    public PerspectiveCamera cam;
+    private Texture roadTexture;
+    public PerspectiveCamera editCam;
+    public PerspectiveCamera driveCam;
     public CameraInputController inputController;
     public ModelBatch modelBatch;
     public Array<ModelInstance> instances;
+    public Array<ModelInstance> debugInstances;
     public Array<Disposable> disposables;
     public Environment environment;
     private ShapeRenderer shapeRenderer;
-    private CatmullRomSpline<Vector3> spline;
+    private CatmullRomSpline<Vector3> positionSpline;
+    private CatmullRomSpline<Vector3> normalSpline;
     private final Vector3[] pathPoints = new Vector3[100];	// to render spline (debug)
+    private float time = 0;
+    private boolean driveMode = false;
 
 
     @Override
@@ -41,6 +47,7 @@ public class Main extends ApplicationAdapter {
 
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        roadTexture = new Texture("road.jpg");
         disposables.add(batch);
         disposables.add(image);
 
@@ -51,48 +58,89 @@ public class Main extends ApplicationAdapter {
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .4f, .4f, .4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
-        cam.lookAt(0, 0, 0);
-        cam.near = 0.1f;
-        cam.far = 150f;
-        cam.update();
+        editCam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        editCam.position.set(10f, 10f, 10f);
+        editCam.lookAt(0, 0, 0);
+        editCam.near = 0.1f;
+        editCam.far = 150f;
+        editCam.update();
+
+        driveCam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        driveCam.position.set(10f, 10f, 10f);
+        driveCam.lookAt(0, 0, 0);
+        driveCam.near = 0.1f;
+        driveCam.far = 150f;
+        driveCam.update();
+
 
         instances = new Array<>();
+        debugInstances = new Array<>();
 
 
         ModelBuilder modelBuilder = new ModelBuilder();
 
         Model gridModel = modelBuilder.createLineGrid(200, 200, 1, 1,new Material(ColorAttribute.createDiffuse(Color.GRAY)),VertexAttributes.Usage.Position );
-        instances.add( new ModelInstance(gridModel));
+        debugInstances.add( new ModelInstance(gridModel));
         disposables.add(gridModel);
 
         Model xyzModel = modelBuilder.createXYZCoordinates(10, new Material(),VertexAttributes.Usage.Position|VertexAttributes.Usage.ColorPacked );
-        instances.add( new ModelInstance(xyzModel));
+        debugInstances.add( new ModelInstance(xyzModel));
         disposables.add(xyzModel);
 
-        Gdx.input.setInputProcessor(new InputMultiplexer( inputController = new CameraInputController(cam)));
+        Gdx.input.setInputProcessor(new InputMultiplexer( inputController = new CameraInputController(editCam)));
 
         buildSpline();
         shapeRenderer = new ShapeRenderer();
+
+        Model roadModel = buildRoadModelFromSpline(positionSpline);
+        instances.add( new ModelInstance(roadModel));
+        disposables.add(roadModel);
+
+        Model lineModel = buildCentreLineModelFromSpline(positionSpline);
+        instances.add( new ModelInstance(lineModel));
+        disposables.add(lineModel);
+
+        placeDriveCamera(driveCam, 0);
     }
 
     @Override
     public void render() {
+        if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_1))
+            driveMode = false;
+        if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_2))
+            driveMode = true;
+        float delta = Gdx.graphics.getDeltaTime();
+        time += 0.02f * delta;
+        if(time > 1.0f)
+            time -= 1.0f;
         inputController.update();
+        placeDriveCamera(driveCam, time);
 
         ScreenUtils.clear(Color.TEAL, true);
 
+        Camera cam = driveMode ? driveCam : editCam;
+
         modelBatch.begin(cam);
         modelBatch.render(instances, environment);
+        if(!driveMode)
+            modelBatch.render(debugInstances, environment);
         modelBatch.end();
 
-        renderSpline();
+        if(!driveMode)
+            renderSpline(cam);
 
 
         batch.begin();
         batch.draw(image, 40, 21);
         batch.end();
+    }
+
+    private void placeDriveCamera( PerspectiveCamera driveCam, float t){
+        positionSpline.valueAt(driveCam.position, t);
+        driveCam.position.y += 0.3f;
+        normalSpline.valueAt(driveCam.up, t);
+        positionSpline.derivativeAt(driveCam.direction, t);
+        driveCam.update();
     }
 
 
@@ -104,14 +152,27 @@ public class Main extends ApplicationAdapter {
             new Vector3(-20*scl, ht, 20*scl),
             new Vector3(20*scl, ht, 25*scl),
 
-            new Vector3(25*scl, ht, -30*scl),
+            new Vector3(25*scl, ht+3f, -30*scl),
 
             new Vector3(-15*scl, ht, -24*scl),
             new Vector3(-50*scl, ht, -5*scl),
 
             new Vector3(-15*scl, ht, 5*scl),
         };
-        spline = new CatmullRomSpline<Vector3>(controlPoints, true);
+        positionSpline = new CatmullRomSpline<Vector3>(controlPoints, true);
+
+        Vector3[] normalControlPoints = {
+            new Vector3(Vector3.Y),
+            new Vector3(Vector3.Y),
+
+            new Vector3(-.25f, 1f, .3f).nor(),
+
+            new Vector3(Vector3.Y),
+            new Vector3(.15f, 1f, .0f).nor(),
+
+            new Vector3(Vector3.Y),
+        };
+        normalSpline = new CatmullRomSpline<Vector3>(normalControlPoints, true);
 
         ModelBuilder modelBuilder = new ModelBuilder();
         Model blockModel = modelBuilder.createBox(1f, 1f, 1f, new Material(ColorAttribute.createDiffuse(Color.BLUE)),
@@ -119,28 +180,151 @@ public class Main extends ApplicationAdapter {
         disposables.add(blockModel);
         for(Vector3 p : controlPoints){
             ModelInstance instance = new ModelInstance(blockModel, p);
-            instances.add(instance);
+            debugInstances.add(instance);
         }
 
         // fill array of points for debug render
         for(int i = 0; i < 100; i++) {
             Vector3 out = new Vector3();
-            spline.valueAt(out, i/100f);
+            positionSpline.valueAt(out, i/100f);
             pathPoints[i] = out;
         }
     }
 
     // render path as a red line (debug)
-    private void renderSpline() {
+    private void renderSpline(Camera cam) {
+        Vector3 normal = new Vector3();
         shapeRenderer.setProjectionMatrix(cam.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(1,0,0,1);
         for(int i = 0; i < 100-1; i++)
         {
             shapeRenderer.line(pathPoints[i], pathPoints[i+1]);
+            normalSpline.valueAt(normal, i/100f);
+            normal.add(pathPoints[i]);
+            shapeRenderer.line(pathPoints[i], normal);
         }
         shapeRenderer.line(pathPoints[99], pathPoints[0]);
         shapeRenderer.end();
+    }
+
+
+    private Model buildRoadModelFromSpline(CatmullRomSpline<Vector3> spline){
+        int attr = VertexAttributes.Usage.Position | VertexAttributes.Usage.TextureCoordinates;
+        int primitive = GL20.GL_TRIANGLES;
+        Material material = new Material(ColorAttribute.createDiffuse(Color.WHITE), TextureAttribute.createDiffuse(roadTexture));
+        int N = 100;
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.begin();
+        MeshBuilder meshBuilder = (MeshBuilder) modelBuilder.part("road", primitive, attr, material);
+
+        int numVerts = 4 * N;
+        int numTris = 2 *N;
+        meshBuilder.ensureVertices(numVerts);
+        meshBuilder.ensureTriangleIndices(numTris);
+
+        Vector3 p1 = new Vector3();
+        Vector3 p2 = new Vector3();
+        Vector3 n1 = new Vector3();
+        Vector3 n2 = new Vector3();
+        Vector3 d1 = new Vector3();
+        Vector3 d2 = new Vector3();
+        Vector3 c0 = new Vector3();
+        Vector3 c1 = new Vector3();
+        Vector3 c2 = new Vector3();
+        Vector3 c3 = new Vector3();
+
+
+        float width = 3f;
+
+        for(int i = 0; i <N; i++){
+            // p1 and p2 : 2 adjacent points on the spline
+
+            spline.valueAt(p1, i/(float)N);
+            spline.valueAt(p2, ((i+1)%N)/(float)N);
+
+            normalSpline.valueAt(n1, i/(float)N);
+            normalSpline.valueAt(n2, ((i+1)%N)/(float)N);
+
+            // get derivative at both points, rotate 90 degrees to get a vector towards the side of the road
+            spline.derivativeAt(d1, i/(float)N);
+            d1.rotate(n1, 90).nor();
+            spline.derivativeAt(d2, (i+1)/(float)N);
+            d2.rotate(n2, 90).nor();
+
+            // calculate 4 corners of a segment
+            c0.set(d1).scl(-width).add(p1);
+            c3.set(d1).scl(width).add(p1);
+            c1.set(d2).scl(-width).add(p2);
+            c2.set(d2).scl(width).add(p2);
+
+            // add rectangle to the mesh
+            meshBuilder.rect(c0, c1, c2, c3, Vector3.Y);
+        }
+        return modelBuilder.end();
+
+    }
+
+    /** create centre line marking as rectangles */
+    private Model buildCentreLineModelFromSpline(CatmullRomSpline<Vector3> spline){
+        int attr = VertexAttributes.Usage.Position | VertexAttributes.Usage.ColorPacked;
+        int primitive = GL20.GL_TRIANGLES;
+        Material material = new Material(ColorAttribute.createDiffuse(Color.LIGHT_GRAY));
+        int N = 100;
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.begin();
+        MeshBuilder meshBuilder = (MeshBuilder) modelBuilder.part("road", primitive, attr, material);
+
+        int numVerts = 4 * N;
+        int numTris = 2 *N;
+        meshBuilder.ensureVertices(numVerts);
+        meshBuilder.ensureTriangleIndices(numTris);
+
+        Vector3 p1 = new Vector3();
+        Vector3 p2 = new Vector3();
+        Vector3 n1 = new Vector3();
+        Vector3 n2 = new Vector3();
+        Vector3 d1 = new Vector3();
+        Vector3 d2 = new Vector3();
+        Vector3 c0 = new Vector3();
+        Vector3 c1 = new Vector3();
+        Vector3 c2 = new Vector3();
+        Vector3 c3 = new Vector3();
+
+
+        float width = 0.05f;
+
+        for(int i = 0; i <N; i++){
+            // p1 and p2 : 2 adjacent points on the spline
+
+            spline.valueAt(p1, i/(float)N);
+            spline.valueAt(p2, ((i+0.2f)%N)/(float)N);
+            p1.y += 0.05f;
+            p2.y += 0.05f;
+
+
+            normalSpline.valueAt(n1, i/(float)N);
+            normalSpline.valueAt(n2, ((i+1)%N)/(float)N);
+
+            // get derivative at both points, rotate 90 degrees to get a vector towards the side of the road
+            spline.derivativeAt(d1, i/(float)N);
+            d1.rotate(n1, 90).nor();
+            spline.derivativeAt(d2, (i+1)/(float)N);
+            d2.rotate(n2, 90).nor();
+
+            // calculate 4 corners of a segment
+            c0.set(d1).scl(-width).add(p1);
+            c3.set(d1).scl(width).add(p1);
+            c1.set(d2).scl(-width).add(p2);
+            c2.set(d2).scl(width).add(p2);
+
+            // add rectangle to the mesh
+            meshBuilder.rect(c0, c1, c2, c3, Vector3.Y);
+        }
+        return modelBuilder.end();
+
     }
 
     @Override
